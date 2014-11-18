eeac8c9f9f1fe5f891fa3e594afa7c8ef46a48ef
julian mueller <jul.mue@hotmail.de>
2014-11-18 11:14:32 +0100

    rewrote the tests to not use internal predicates. this should allow for easy implementation of the parser.

--- parser_internal_tests.pl
+++ parser_internal_tests.pl
@@ -1,68 +1,42 @@
 :- use_module(parser_internal).
 
 
-
-:- begin_tests(alphabet).
-
-test(var_1) :-
-    v(['x'],[]).
-
-
-:- end_tests(alphabet).
-
-
-:- begin_tests(expr_rest).
-
-test(expr_rest_1) :-
-    expr_rest(end,[],[]).
-
-test(expr_rest_2) :-
-    expr_rest(and,['&&','v'],[]).
-
-test(expr_rest_3) :-
-    expr_rest(or,['||','v'],[]).
-
-:- end_tests(expr_rest).
-
-
-
-:- begin_tests(expr).
-
-test(expr_1) :-
-    expr(['x'],[]).
-
-test(expr_2) :-
-    expr(['~','x'],[]).
-
-test(expr_3) :-
-    expr(['x','&&','x'],[]).
-
-test(expr_4) :-
-    expr(['x','||','x'],[]).
-
-test(expr_5) :-
-    expr(['(','x',')'],[]).
-
-test(expr_6) :-
-    expr(['(','x',')','&&','x'],[]).
-
-:- end_tests(expr).
-
-/*
 :- begin_tests(recogniser).
 
-test(recogniser_var_1) :-
-    rec_expr(_,['x'],[]).
+test(var_1) :-
+    rec_expr(['x']).
 
-test(parser_neg_1) :-
-    rec_expr(_,['~','x'],[]).
+/*
+test(expr_rest_1) :-
+    rec_expr(end,[],[]).
 
-test(parser_and_1) :-
-    rec_expr(_,['x','&&','x']).
+test(expr_rest_2) :-
+    rec_expr(and,['&&','v'],[]).
 
+test(expr_rest_3) :-
+    rec_expr(or,['||','v'],[]).
+
+test(expr_1) :-
+    rec_expr(['x'],[]).
+*/
+
+test(expr_2) :-
+    rec_expr(['~','x']).
+
+test(expr_3) :-
+    rec_expr(['x','&&','x']).
+
+test(expr_4) :-
+    rec_expr(['x','||','x']).
+
+test(expr_5) :-
+    rec_expr(['(','x',')']).
+
+test(expr_6) :-
+    rec_expr(['(','x',')','&&','x']).
 
 :- end_tests(recogniser).
-*/
+
 
 /*
 
